5b677365141be21d5b9654a871eae977b887e3ab
Meloz <1001234001@mail.ru>
2020-03-08 17:56:01 +0300

    add new files

---
+++ include/DateHeader.h
@@ -0,0 +1,40 @@
+#ifndef INCLUDE_DateHeader_H_
+#define INCLUDE_DateHeader_H_
+#endif
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+class Date {
+private:
+	int y = 0, m = 0, d = 0;
+
+public:
+	Date();
+	Date(int _d, int _m, int _y);
+	Date(const char str[]);
+	friend ostream& operator<<(ostream& stream, const Date& c); //перегрузка вывода
+	friend istream& operator>>(istream& stream, Date& c); //перегрузка ввода
+	Date operator+(int d);
+	Date operator-(int d);
+	bool operator==(const Date& c);
+
+	friend bool operator==(const Date& left, const Date& right) {
+		if (left.d == right.d && left.m == right.m && left.y == right.y) {
+			return 1; 
+		}
+		else
+			return 0;
+	}
+
+	Date& operator=(const Date& c);
+	bool operator>(const Date& c);
+	bool operator<(const Date& c);
+	string stringdatemethod(const Date& c);
+	int getDay();
+	int getMonth();
+	int getYear();
+	~Date();
+};
---
+++ test/test_Date.cpp
@@ -0,0 +1,93 @@
+﻿#include <gtest/gtest.h>
+#include "DateHeader.h"
+
+TEST(TestClassDate , Addintdate) {
+  Date p1("05.09:1917");
+  int i = 15;
+  Date result("20.09:1917"); 
+  const Date p_ = p1 + i;
+  EXPECT_EQ(p_, result);
+}
+TEST(TestClassDate, subintdate) {
+  Date p1("05.09:1917");
+  int i = 15;
+  Date result("20.08:1917");
+  const Date p_ = p1 - i;
+  EXPECT_EQ(p_, result);
+}
+TEST(TestClassDate, equaly) {
+  Date p1("5.9:1917");
+  Date p2("5.9:1917");
+  bool i;
+  if (p1 ==p2) {
+    i = 1;
+  }
+  EXPECT_EQ(i, 1);
+}
+TEST(TestClassDate, more) {
+  Date p1("6.9:1917");
+  Date p2("5.9:1917");
+  bool i;
+  if (p1 > p2) {
+    i = 1;
+  }
+  EXPECT_EQ(i, 1);
+}
+TEST(TestClassDate, less) {
+  Date p1("4.9:1917");
+  Date p2("6.9:1917");
+  bool i;
+  if (p1 < p2) {
+    i = 1;
+  }
+  EXPECT_EQ(i, 1);
+}
+TEST(TestClassDate, twodatesassign) {
+  Date p1("4.9:1984");
+  Date p2("4.9:2000");
+  Date result("4.9:2000");
+  p1 = p2;
+  EXPECT_EQ(p1, result);
+}
+TEST(TestClassDate, can_use_default_constructor) {
+  Date p1;
+  int i = 0;
+  if ((p1.getDay() == 0) && (p1.getMonth() == 0) && (p1.getYear() == 0)) {
+     i = 1;
+  }
+  EXPECT_EQ(i, 1);
+}
+TEST(TestClassDate, can_use_initializator_constructor) {
+  int i = 0;
+  Date p1("4.9:2000");
+  if (((int)(p1.getDay()) == 4) && (p1.getMonth() == 9) && (p1.getYear() == 2000)) {
+     i = 1;
+  }
+  EXPECT_EQ(i, 1);
+}
+TEST(TestClassDate, can_use_converter_constructor) {
+  Date p1("2.4:2077"); 
+  int k;
+  if ((p1).stringdatemethod(p1) == "2.4:2077") {
+    k = 1;
+  }
+  EXPECT_EQ(k, 1);
+}
+TEST(TestClassDate, GetDay) {
+  Date p1("02.04:2076");
+  int k;
+  k = p1.getDay();
+  EXPECT_EQ(k, 2);
+}
+TEST(TestClassDate, GetMonth) {
+  Date p1("02.04:2076");
+  int k;
+  k = p1.getMonth();
+  EXPECT_EQ(k, 4);
+}
+TEST(TestClassDate, GetYear) {
+  Date p1("02.04:2076");
+  int k;
+  k = p1.getYear();
+  EXPECT_EQ(k, 2076);
+}
